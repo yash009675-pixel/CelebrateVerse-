0eca1c59a66c323811da35a95907fb61f38eb699
Yash Makwana <yash.009675@gmail.com>
2026-08-31 20:32:35 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,143 @@
+const CACHE_NAME = "celebrateverse-v1";
+
+
+const ASSETS = [
+
+    "./",
+
+    "./index.html",
+
+    "./customize.html",
+
+    "./payment.html",
+
+    "./style.css",
+
+    "./main.js",
+
+    "./customize.js",
+
+    "./payment.js",
+
+    "./supabase.js"
+
+];
+
+
+/* =========================
+   INSTALL SERVICE WORKER
+========================= */
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(
+                CACHE_NAME
+            )
+
+            .then(
+                cache => {
+
+                    return cache.addAll(
+                        ASSETS
+                    );
+
+                }
+            )
+
+        );
+
+
+        self.skipWaiting();
+
+    }
+);
+
+
+/* =========================
+   ACTIVATE SERVICE WORKER
+========================= */
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+
+            .then(
+                keys => {
+
+                    return Promise.all(
+
+                        keys.map(
+                            key => {
+
+                                if (
+                                    key !==
+                                    CACHE_NAME
+                                ) {
+
+                                    return caches.delete(
+                                        key
+                                    );
+
+                                }
+
+                            }
+                        )
+
+                    );
+
+                }
+            )
+
+        );
+
+
+        self.clients.claim();
+
+    }
+);
+
+
+/* =========================
+   FETCH FILES
+========================= */
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            fetch(
+                event.request
+            )
+
+            .then(
+                response => {
+
+                    return response;
+
+                }
+            )
+
+            .catch(
+                () => {
+
+                    return caches.match(
+                        event.request
+                    );
+
+                }
+            )
+
+        );
+
+    }
+);
