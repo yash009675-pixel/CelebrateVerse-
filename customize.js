28934ead488668413bad924899bce6291fe79110
Yash Makwana <yash.009675@gmail.com>
2026-09-05 00:47:56 +0530

    Restore stable pre-refactor version to stop editor runtime failures

--- customize.js
+++ customize.js
@@ -1302,7 +1302,6 @@ document.addEventListener("DOMContentLoaded", () => {
             if (error) throw error;
             celebration = data;
             currentDraftId = celebration.id;
-            try { localStorage.setItem("celebrateVerseProjectId", celebration.id); localStorage.setItem("celebrateVerseDraftId", celebration.id); } catch(e) {}
         }
 
         const photoPaths = await uploadPhotos(user.id, celebration.id);
